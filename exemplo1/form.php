7c0e72c31ee9f8497544ef0b71fd5274dc36ce22
ItsNiSantana <bianewlife@gmail.com>
2024-06-18 11:21:27 -0300

    hi

---
+++ exemplo1/form.php
@@ -0,0 +1,14 @@
+<?php
+    echo "<h2> Cadastro <h2>";
+
+    //$_GET['nome_campo_forma_html']
+    //$_POST['nome_campo_forma_html']
+
+    //recebendo dado enviado
+    //do form html via GET
+
+    $nome = $_GET['nome'];
+
+    echo "<h3>Nome:<h3>";
+    echo $nome;
+?>
