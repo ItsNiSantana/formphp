001203a8ffe0be56dc6e1fc7d04e03b79e5424ed
Nicoli <103062064+ItsNiSantana@users.noreply.github.com>
2024-06-25 09:14:08 -0300

    Update form.php
    Signed-off-by: Nicoli <103062064+ItsNiSantana@users.noreply.github.com>

--- exercicio12(nao feito ainda)/form.php
+++ exercicio12(nao feito ainda)/form.php
@@ -1,4 +1,17 @@
 <?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+      $pasta = $_POST["pasta"];
+      $n_imagens = $_POST["num_imagens"];
 
+      echo "<table>";
+      echo "<tr>";
 
-?>
+      for($i = 1; $i <= $n_imagens; $i++) {
+        echo "<td>";
+        echo "<img src='http://localhost/$pasta/$i.jpg' width='150' height='100'>";
+        echo "</td>";
+      }
+
+      echo "</tr>";
+      echo "</table>";
+    }
